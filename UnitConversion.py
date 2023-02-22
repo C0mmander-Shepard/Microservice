27813aea818dac884a171f5e88b8912ce4788134
C0mmander-Shepard <38335751+C0mmander-Shepard@users.noreply.github.com>
2023-02-21 22:30:52 -0800

    Operational Microservice
    Updated to continuously run once started.

--- UnitConversion.py
+++ UnitConversion.py
@@ -6,18 +6,18 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:7777")
 
-user_data = socket.recv_json()
-print("Request received")
+while True:
+    user_data = socket.recv_json()
+    print("Request received")
 
-#convert lbs to kg
-user_data[0] = user_data[0] * 0.45359237
+    #convert lbs to kg
+    user_data[0] = user_data[0] * 0.45359237
 
-user_data[1] = user_data[1] * 2.54
+    user_data[1] = user_data[1] * 2.54
 
-print(user_data)
+    print(user_data)
 
-time.sleep(1)
-print("Sending response...")
+    time.sleep(1)
+    print("Sending response...")
 
-socket.send_json(user_data)
-
+    socket.send_json(user_data)
